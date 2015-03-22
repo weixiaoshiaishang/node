f891e993d198062a8f56ec7e571e9f9b626f4e31
zhangrenyang-t510 <zhangrenyang_ios@126.com>
2015-03-22 16:02:07 +0800

    'init'

--- zfblog/model/user.js
+++ zfblog/model/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('../db');
+var crypto = require('crypto');
 //声明userSchema,用来定义collection的名字和里面文档的存储结构
 var userSchema = new mongoose.Schema({
     username:String,
@@ -16,6 +17,25 @@ function User(user){
     this.email = user.email;
 }
 
+User.prototype.save = function(callback){
+    var md5 = crypto.createHash('md5'),
+    emailMd5 = md5.update(this.email.toLowerCase()).digest('hex'),
+    avatar = "https://secure.gravatar.com/avatar/"+emailMd5+"?s=48";
+
+    var newUser = new userModel({
+        username:this.username,
+        password:this.password,
+        email:this.email,
+        avatar:avatar
+    });
+
+    newUser.save(function(err,user){
+        if(err)
+            callback(err);
+        else
+            callback(null,user);
+    });
+}
 
 
 
