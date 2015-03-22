de65207ac061c3597bc7b66cfea0440d98f72642
zhangrenyang-t510 <zhangrenyang_ios@126.com>
2015-03-22 12:49:19 +0800

    init

--- zfblog/app.js
+++ zfblog/app.js
@@ -4,13 +4,17 @@ var favicon = require('serve-favicon');//收藏图标中间件
 var logger = require('morgan');//日志中间件
 var cookieParser = require('cookie-parser');//解析cookie
 var bodyParser = require('body-parser');//解析body的数据
-
+var session = require('express-session');
+var flash = require('connect-flash');
 var routes = require('./routes/index');//首页
 var users = require('./routes/users');//用户
 var article = require('./routes/article');
 
 var app = express();
-
+app.use(session({
+  secret:'zfblog'
+}));
+app.use(flash());
 // 设置模板引擎和模板存放路径
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,7 +27,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());//解析 cookie
 app.use(express.static(path.join(__dirname, 'public')));//静态文件中间件
-
+app.use(function(req,res,next){
+  req.locals = {
+    error:""
+  }
+  next();
+})
 app.use('/', routes);
 
 app.use('/users', users);
--- zfblog/routes/users.js
+++ zfblog/routes/users.js
@@ -4,12 +4,20 @@ var router = express.Router();
 
 router.get('/reg', function(req, res, next) {
   res.render('user/reg',{
-    title:"欢迎注册"
+    title:"欢迎注册",
+    er:req.flash('error').toString()
   });
 });
 
 router.post('/reg', function(req, res, next) {
-  //res.send(req.body);
+  var username = req.body.username,
+      email = req.body.email,
+      password = req.body.password,
+      password_repeat = req.body['password_repeat'];
+  if(!username){
+    req.flash('error','用户名不能为空');
+    return res.redirect('back');
+  }
   res.redirect('/');
 });
 
