9504826734ef4a93b1caca9225afb9a60724f091
zhangrenyang-t510 <zhangrenyang_ios@126.com>
2015-03-24 14:04:09 +0800

    俊杰帮忙改进了cookieutil

--- cookie/cookieUtils.js
+++ cookie/cookieUtils.js
@@ -1,53 +1,71 @@
+/**
+ * Created by sunjunjie on 15/3/24.
+ */
 
 
-module.exports.parse = function(str){
-    var cookieObj = {};
+function parse(str){
+    var cookieObj = {}
     if(str){
-        var vals = str.split('; ');
+        var vals = str.split("; ");
         for(var i=0;i<vals.length;i++){
-            var kv = vals[i].split('=');
+            var kv = vals[i].split("=");
             cookieObj[kv[0]] = decodeURIComponent(kv[1]);
         }
     }
     return cookieObj;
 }
-/**
- * 把对象转化成发送到客户端的cookie字符串
- * @param cookieObj 对象
- * @param options 选项
- */
-module.exports.toArray = function(cookieObj,options){
-    var cookieArr = [];//Set-Cookie数组
-    if(cookieObj){
-        for(var name in cookieObj){//迭代对象的每个属性
-            var cookieStr = name +"="+encodeURIComponent(cookieObj[name]);
-            if(options){
-                if(options.path)
-                    cookieStr+=";path="+options.path;
-                if(options.maxAge){
-                    var maxAge = parseInt(options.maxAge);
-                    if(isNaN(maxAge)){
-                        throw new Error('maxAge must be a number');
-                    }else{
-                        cookieStr += ';max-age='+maxAge;
-                    }
+
+
+function toArray(){  //[{name:"珠峰培训",age:6},{path:"/read1"}],[{name:"jeason",age:18,sex:"male"},{path:"/read2"}]
+    var cookieArray =[];
+    for(var i=0;i<arguments.length;i++){
+        var cookies = arguments[i];
+        var cookieStr = tostring(cookies);
+        Array.prototype.push.apply(cookieArray, cookieStr);
+    }
+    console.log(cookieArray);
+    return cookieArray;
+}
+
+function tostring(cookies){ //[{name:"珠峰培训",age:6},{path:"/read1"}]
+    var cookieArr = [];
+    var cookieObj = cookies[0];
+    for(name in cookieObj) {
+        var cookieStr = name + "=" + encodeURIComponent(cookieObj[name]);
+        if (cookies[1]) {
+            var options = cookies[1];
+            if (options.path) {
+                cookieStr += ";path=" + options.path;
+            }
+            if (options.maxAge) {
+                var maxAge = parseInt(options.maxAge);
+                if (isNaN(maxAge)) {
+                    throw new Error("maxAge must be a number");
+                } else {
+                    cookieStr += ";max-age=" + maxAge;
                 }
-                if(options.domain){
-                    cookieStr += ';domain='+options.domain;
+            }
+            if (options.domain) {
+                cookieStr += ";domain=" + options.domain;
+            }
+            if (options.expires) {
+                if (options.expires instanceof  Date) {
+                    cookieStr += ";expires=" + options.expires.toLocaleString();
+                } else {
+                    throw new Error("must be a date");
                 }
-                if(options.expires){
-                    if(options.expires instanceof Date){
-                        cookieStr += ';expires='+options.expires.toLocaleString();
-                    }else{
-                        throw new Error('must be a date');
-                    }
-                }
-                if(options.httpOnly){
-                    cookieStr += ";httpOnly";
-                }
-                cookieArr.push(cookieStr);
+            }
+            if (options.httpOnly) {
+                cookieStr += ";httpOnly";
+
             }
         }
+        cookieArr.push(cookieStr);
     }
+//    console.log("==="+cookieArr);
     return cookieArr;
 }
+
+
+module.exports.parse = parse;
+module.exports.toArray = toArray;
--- cookie/cookie_02.js
+++ cookie/cookie_02.js
@@ -12,7 +12,7 @@
 
 var url = require('url');
 var http = require('http');
-var cookieUtils = require('./cookieUtils');
+var cookieUtils = require('./cookieUtils2');
 http.createServer(function(req,res){
     var urlObj = url.parse(req.url);
     if('/favicon.ico' == urlObj.pathname){
@@ -21,13 +21,14 @@ http.createServer(function(req,res){
     }else if("/write" == urlObj.pathname){
         res.writeHead(200,{
             "Content-Type":"text-html;charset=utf-8",
-            "Set-Cookie":cookieUtils.toArray({name:"珠峰",age:6},{
+            /*"Set-Cookie":cookieUtils.toArray({name:"珠峰",age:6},{
                 //path:"/path2",
                maxAge:'3',//秒
                 //domain:'localhost',
                 expires:new Date(new Date().getTime()+30*1000)//,
                 //httpOnly:false
-            })
+            })*/
+            "Set-Cookie":cookieUtils.toArray([{name:"珠峰培训",age:6},{path:"/read1"}],[{name:"jeason",age:18,sex:"male"},{path:"/read2"}])
         })
         res.end('OK');
     }else{
